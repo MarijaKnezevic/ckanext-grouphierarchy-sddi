6f019c7362028e66f47442c9e4aea03f9f22e1f9
Marija <93824048+MarijaKnezevic@users.noreply.github.com>
2023-12-15 13:19:34 +0100

    Update cli.py

--- ckanext/grouphierarchy/cli.py
+++ ckanext/grouphierarchy/cli.py
@@ -24,44 +24,35 @@ def grouphierarchy():
     pass
 
 
-def create_org_or_group(group, is_org=False):
+@grouphierarchy.command("init_data")
+def init_data():
+
+    data = get_init_data()
     user = tk.get_action('get_site_user')({'ignore_auth': True})
-    context = {
+
+    for group in data:
+        context = {
             "model": model,
             "session": model.Session,
             "user": user['name'],
             "return_id_only": True
         }
+        if group.get('image_url'):
+            group['image_url'] = _site_url + group.get('image_url')
 
-    grp_or_org = 'organization_create' if is_org else 'group_create'
-    if group.get('image_url'):
-        group['image_url'] = _site_url + group.get('image_url')
-
-    try:
-        tk.get_action(grp_or_org)(context, group)
-    except (NotFound, NotAuthorized) as e:
-        error_shout(e)
-        raise click.Abort()
-    except dict_fns.DataError:
-        error_shout('Integrity Error')
-        raise click.Abort()
-    except ValidationError as e:
-        error_shout(e)
-        raise click.Abort()
-
-
-@grouphierarchy.command("init_data")
-def init_data():
-
-    data = get_init_data()
-
-    for group in data.get('groups'):
-        create_org_or_group(group)
-    for org in data.get('organizations'):
-        create_org_or_group(org, is_org=True)
-
+        try:
+            tk.get_action('group_create')(context, group)
+        except (NotFound, NotAuthorized) as e:
+            error_shout(e)
+            raise click.Abort()
+        except dict_fns.DataError:
+            error_shout('Integrity Error')
+            raise click.Abort()
+        except ValidationError as e:
+            error_shout(e)
+            raise click.Abort()
     click.secho(
-        "Successfully created the initial Orgs/Groups",
+        "Successfully created the initial Groups",
         fg="green",
         bold=True
     )
